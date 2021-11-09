61bc9e4ee84769230cff0a61d5ff91271a2cf19d
Vustah <66439850+Vustah@users.noreply.github.com>
2021-11-09 13:33:22 +0100

    Add files via upload

--- rename_file_version.bat
+++ rename_file_version.bat
@@ -1,6 +1,3 @@
 @echo %off
 
 python "%~dp0\rename_file_versions.py" %1 %2
-goto :finished 
-:finished 
-echo ---------------------------------------------------------------------------
--- rename_file_versions.py
+++ rename_file_versions.py
@@ -1,15 +1,30 @@
 import sys
 import os
+from termcolor import colored
 
 old_version = sys.argv[1]
 new_version = sys.argv[2]
 
 path = os.getcwd()
-for root, directories, files in os.walk(path, topdown=False):
+current_folder = ""
+for root, directories, files in os.walk(path, topdown=True):
+    Current_dir = path.split("\\")[-1]
+    splitted_root = root.split("\\")
+    current_dir_index = splitted_root.index(Current_dir)
+    current_folder_new = "\\".join(splitted_root[current_dir_index:])    
     for name in files:
+        max_file_length = len(max(files,key=len))
         if old_version in name:
             old_name = os.path.join(root, name)
-            new_name = name.replace(old_version,new_version)
-            new_name = os.path.join(root, new_name)
-            os.rename(old_name, new_name)
-            print(old_name, " --> ", new_name)
+            new_name_simple = name.replace(old_version,new_version)
+            new_name = os.path.join(root, new_name_simple)
+            if current_folder_new != current_folder:
+                current_folder = current_folder_new
+                print("\n-->", colored(current_folder_new,'red', attrs=['bold']))
+            try:
+                os.rename(old_name, new_name)
+            except FileExistsError as e:
+                print("---->", colored(new_name_simple+" already exists",attrs=['bold']))
+            else:
+                spacing = (max_file_length-len(new_name_simple))*" "
+                print("---->", name, spacing, "-->", new_name_simple)
